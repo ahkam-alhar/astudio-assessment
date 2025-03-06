7857ad34b02ec597e47418c66086b6f22cb629fe
Ahkam Alhar <maaahkam@gmail.com>
2025-03-06 23:21:59 +0530

    chore(config): configure tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    container: {
+      screens: {
+        sm: '100%',
+        md: '100%',
+        lg: '1224px',
+        xl: '1224px',
+        '2xl': '1224px',
+      },
+      padding: {
+        DEFAULT: '16px',
+      },
+      center: true,
+    },
+    fontFamily: {
+      sans: ['Neutra Text', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        black: '#322625',
+        grey: '#ebebeb',
+        blue: '#c0e3e5',
+        yellow: '#fdc936',
+      },
+    },
   },
   plugins: [],
 };
